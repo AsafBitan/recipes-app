1c3e7cd1bffa8c5da5ba5b2b953927b11b302825
AsafBitBGU <asafbit@post.bgu.ac.il>
2024-09-03 11:55:36 +0300

    recipe project in javascript

--- recipes_project/recipes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RecipeSearchView, FavoriteListView
-
-urlpatterns = [
-    path('search/', RecipeSearchView.as_view(), name='recipe-search'),
-    path('favorites/', FavoriteListView.as_view(), name='favorite-list'),
-]
